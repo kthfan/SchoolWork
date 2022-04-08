2ee63cdb0e5cf38e6f82b51eeaf911fec14b972b
kthfan <58523053+kthfan@users.noreply.github.com>
2022-04-09 01:18:46 +0800

    Update table2image.py

--- Computer Science/Image Processing/table2image.py
+++ Computer Science/Image Processing/table2image.py
@@ -1,10 +1,38 @@
 class Table:
-    def __init__(self, row_size_list=[1], col_size_list=[1], line_width=1, border_color=(0,0,0)):
+    def __init__(self):
+        return
+    def draw_text(input_img=None):
+        return
+    def put_table(self, row, col, table):
+        return
+    def put_text(self, row, col, item):
+        return
+    def get_tabel_dim(self):
+        return
+    def draw_text(self, line_text, input_img, y1, y2, x1, x2):
+        line_text = line_text.split('\n')
+        width, height = 0, 0
+        dim_list = np.zeros((2, len(line_text)), dtype=np.int32)
+        for line, text in enumerate(line_text):
+            tw, th = cv2.getTextSize(text=text, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, thickness=1)[0]
+            height += th;
+            dim_list[1, line] = height;
+            left = x1 + (x2 - x1 - tw) // 2
+            dim_list[0, line] = left; 
+        center_y = y2 - (y2 - y1 + height) // 2
+        dim_list[1] += center_y
+        for line, text in enumerate(line_text):
+            input_img = cv2.putText(input_img, text, (dim_list[0, line], dim_list[1, line]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
+            
+class LineTable(Table):
+    def __init__(self, row_size_list=[1], col_size_list=[1], line_width=1, border_color=(0,0,0), img_shape=(256, 256)):
+        super().__init__()
         self.line_width = line_width
         self.border_color = border_color
         self.regrid(row_size_list, col_size_list)
         self.sub_tables = dict()
         self.table_entries = dict()
+        self.img_shape = img_shape
     def _compute_grid(self, row_size_list, col_size_list, line_width):
         row_size_list, col_size_list = np.array(row_size_list), np.array(col_size_list)
         row_size_list, col_size_list = row_size_list*(1-line_width) / row_size_list.sum(), col_size_list*(1-line_width) / col_size_list.sum()
@@ -27,23 +55,20 @@ class Table:
         self.sub_tables["{}:{}".format(row, col)] = table
     def put_text(self, row, col, item):
         self.table_entries["{}:{}".format(row, col)] = str(item)
+    def get_tabel_dim(self, row=None, col=None, iput_shape=None):
+        if iput_shape is None: iput_shape = self.img_shape
+        if row is None: row = np.arange(1, len(self.row_size_list))
+        if col is None: col = np.arange(1, len(self.col_size_list))
+        if isinstance(row, int): row = np.array([row])
+        if isinstance(col, int): col = np.array([col])
+        
+        row_size_list, col_size_list = np.array(self.row_size_list), np.array(self.col_size_list)
+        row_size_list, col_size_list = row_size_list*iput_shape[0] / row_size_list.sum(), col_size_list*iput_shape[1] / col_size_list.sum()
+        return row_size_list[row].sum(), col_size_list[col].sum()
     
-    def draw_text(self, line_text, input_img, y1, y2, x1, x2):
-        line_text = line_text.split('\n')
-        width, height = 0, 0
-        dim_list = np.zeros((2, len(line_text)), dtype=np.int32)
-        for line, text in enumerate(line_text):
-            tw, th = cv2.getTextSize(text=text, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, thickness=1)[0]
-            dim_list[1, line] = height;
-            height += th;
-            left = x1 + (x2 - x1 - tw) // 2
-            dim_list[0, line] = left; 
-        center_y = y1 + (y2 - y1 + height) // 2
-        dim_list[1] += center_y
-        for line, text in enumerate(line_text):
-            input_img = cv2.putText(input_img, text, (dim_list[0, line], dim_list[1, line]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
+    
     def draw_table(self, input_img=None):
-        if input_img is None: input_img = np.ones((255, 255, 3), dtype=np.float32)
+        if input_img is None: input_img = np.ones((*img_shape, 3), dtype=np.float32)
         M, N = input_img.shape[0], input_img.shape[1]
         row_size_list, col_size_list, line_width, border_color = self.row_size_list, self.col_size_list, self.line_width, self.border_color
         
@@ -70,7 +95,7 @@ class Table:
             self.draw_text(text, input_img, y1, y2, x1, x2)
         return input_img
     
-class TransparentTable(Table):
+class TransparentTable(LineTable):
     def draw_table(self, input_img=None):
         if input_img is None: input_img = np.ones((255, 255, 3), dtype=np.float32)
         M, N = input_img.shape[0], input_img.shape[1]
@@ -81,7 +106,7 @@ class TransparentTable(Table):
         row_size_list, col_size_list = np.concatenate([np.zeros(1), row_size_list.cumsum()]), np.concatenate([np.zeros(1), col_size_list.cumsum()])
         row_size_list, col_size_list = row_size_list.astype(np.int32), col_size_list.astype(np.int32)
         border_color = np.array(border_color)
-        
+
         for yx, table in self.sub_tables.items():
             [y, x] = [int(i) for i in yx.split(':')]
             y1, y2 = row_size_list[y-1], row_size_list[y]+line_width
@@ -92,18 +117,19 @@ class TransparentTable(Table):
             [y, x] = [int(i) for i in yx.split(':')]
             y1, y2 = row_size_list[y-1], row_size_list[y]+line_width
             x1, x2 = col_size_list[x-1], col_size_list[x]+line_width
-            
+            self.draw_text(text, input_img, y1, y2, x1, x2)
         return input_img
     
-class FlexTable:
+class FlexTable(Table):
     def __init__(self, rows=1, cols=1, line_width=1, border_color=(0,0,0)):
+        super().__init__()
         self.line_width = line_width
         self.border_color = border_color
         self.regrid(rows, cols)
         self.sub_tables = dict()
         self.table_entries = dict()
     def _compute_dim(self, elem):
-        if isinstance(elem, [FlexTable, Table]):
+        if isinstance(elem, Table):
             return elem.get_tabel_dim()
         return self._compute_text_dim(elem)
     def _compute_text_dim(self, text):
@@ -121,7 +147,7 @@ class FlexTable:
         self.sub_tables["{}:{}".format(row, col)] = table
     def put_text(self, row, col, item):
         self.table_entries["{}:{}".format(row, col)] = str(item)
-    def get_tabel_dim(row=None, col=None):
+    def get_tabel_dim(self, row=None, col=None):
         if row is None: row = np.arange(1, self.rows)
         if col is None: col = np.arange(1, self.cols)
         if isinstance(row, int): row = [row]
@@ -132,9 +158,37 @@ class FlexTable:
         for i,r in enumerate(row):
             for j,c in enumerate(col):
                 yx = "{}:{}".format(r, c)
-                elem = self.sub_tablesa.get(yx) or self.table_entries.get(yx)
+                elem = self.sub_tables.get(yx) or self.table_entries.get(yx)
+                if elem is None: continue
                 h, w  = self._compute_dim(elem)
                 result[0, i, j] = h; result[1, i, j] = w
         h, w = result[0].max(axis=1).sum(), result[1].max(axis=0).sum()
         return (h, w)
     
+    
+    def draw_table(self, input_img=None):
+        if input_img is None: input_img = np.ones((255, 255, 3), dtype=np.float32)
+        M, N = input_img.shape[0], input_img.shape[1]
+        line_width = self.line_width
+        
+        row_size_list = np.array([self.get_tabel_dim(row=row)[0] for row in range(self.rows)]).astype(np.int32)
+        col_size_list = np.array([self.get_tabel_dim(col=col)[1] for col in range(self.cols)]).astype(np.int32)
+        
+        border_color = np.array(self.border_color)
+
+        for y in row_size_list: input_img[y:y+line_width, :, :] = border_color
+        for x in col_size_list: input_img[:, x:x+line_width, :] = border_color
+        
+        for yx, table in self.sub_tables.items():
+            [y, x] = [int(i) for i in yx.split(':')]
+            y1, y2 = row_size_list[y-1], row_size_list[y]+line_width
+            x1, x2 = col_size_list[x-1], col_size_list[x]+line_width
+            input_img[y1:y2, x1:x2] = table.draw_table(input_img=input_img[y1:y2, x1:x2])
+        
+        for yx, text in self.table_entries.items():
+            [y, x] = [int(i) for i in yx.split(':')]
+            y1, y2 = row_size_list[y-1], row_size_list[y]+line_width
+            x1, x2 = col_size_list[x-1], col_size_list[x]+line_width
+            self.draw_text(text, input_img, y1, y2, x1, x2)
+        return input_img
+        
